f053219a6acbe459e71bed03521715e8141a1439
Lucas Lopes de Sousa <lucaslopes150995@gmail.com>
2021-04-07 16:47:48 -0300

    Aulas finais
    Instalando o Breeze e começando a aplicar CSS e JavaScript

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ Route::delete('/posts/{id}',[PostController::class, 'destroy'])-> name('posts.de
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::put('/posts/{id}', [PostController::class, 'update'])-> name('posts.update');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+require __DIR__.'/auth.php';
+
