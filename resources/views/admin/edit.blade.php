22934dd48b3030ecd045134eb506196091d63e14
Lucas Lopes de Sousa <lucaslopes150995@gmail.com>
2021-04-07 17:42:30 -0300

    Fim do Curso

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -1,14 +1,13 @@
 @extends('admin.layouts.app')
-
 @section('title','Atualizar Post')
-
 @section('content')
 
-<h1>Edição de Posts <strong>{{ $post->title }}</strong></h1>
-
-<form action="{{ route('posts.update', $post->id) }}" method="post" enctype="multipart/form-data">
-    @method('put')
-    @include('admin._partials.form')
-</form>
+    <h1 class="text-center text-3xl uppercase font-black my-4">Editar de Post: <strong>{{ $post->title }}</strong></h1>
+    <div class="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12 mx-auto">
+        <form action="{{ route('posts.update', $post->id) }}" method="post" enctype="multipart/form-data">
+            @method('put')
+            @include('admin._partials.form')
+        </form>
+    </div>
 
 @endsection
